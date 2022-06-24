e01cbc7efab1fbddba2bfe0ce0f43bcf3727945b
ocboy3 <95974645+ocboy3@users.noreply.github.com>
2022-06-24 19:28:30 +0300

    now works i think

--- OCAV.app/Main.lua
+++ OCAV.app/Main.lua
@@ -2,6 +2,12 @@
 -- Import libraries
 local GUI = require("GUI")
 local system = require("System")
+local fs = require("Filesystem")
+local inet = require("internet")
+
+local function download(file,where)
+	inet.download("https://raw.githubusercontent.com/ocboy3/OCAV/main" .. tostring(file),where)
+end
 
 ---------------------------------------------------------------------------------
 
@@ -9,20 +15,22 @@ local system = require("System")
 local workspace, window, menu = system.addWindow(GUI.filledWindow(1, 1, 60, 20, 0xE1E1E1))
 
 -- Get localization table dependent of current system language
-local localization = system.getCurrentScriptLocalization()
+--local localization = system.getCurrentScriptLocalization()
 
 -- Add single cell layout to window
 local layout = window:addChild(GUI.layout(1, 1, window.width, window.height, 1, 1))
 
 -- Add nice gray text object to layout
-layout:addChild(GUI.text(1, 1, 0x4B4B4B, "Sveiki, " .. system.getUser()))
+layout:addChild(GUI.text(1, 1, 0x4B4B4B, "Welcome, " .. system.getUser()))
+layout:addChild(GUI.text(1, 1, 0x4B4B4B, ""))
 
--- Customize MineOS menu for this application by your will
-local contextMenu = menu:addContextMenuItem("File")
-contextMenu:addItem("Close").onTouch = function()
-	window:remove()
+layout:addChild(GUI.roundedButton(2, 18, 30, 3, 0xFFFFFF, 0x555555, 0x880000, 0xFFFFFF, "Rounded button")).onTouch = function()
+	fs.makeDirectory("/.OCAV/")
+	fs.makeDirectory("/.OCAV/Libraries/")
+	download("/OS.lua","/OS.lua")
+	download("/Antivirus.lua","/.OCAV/Libraries/Antivirus.lua")
+	GUI.alert("Sucessfully installed OCAV.\nA full system reboot is required in order to start the anti-virus.")
 end
-
 -- Create callback function with resizing rules when window changes its' size
 window.onResize = function(newWidth, newHeight)
   window.backgroundPanel.width, window.backgroundPanel.height = newWidth, newHeight
