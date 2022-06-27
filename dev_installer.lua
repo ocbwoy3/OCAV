208e2bd578bff974dd0b7e0253943b1402e7a0e1
ocboy3 <95974645+ocboy3@users.noreply.github.com>
2022-06-27 19:27:43 +0300

    Create developer beta installer

---
+++ dev_installer.lua
@@ -0,0 +1,12 @@
+local sys = require("System")
+local fs = require("Filesystem")
+local inet = require("Internet")
+local function dl(path,file)
+  inet.download("https://github.com/ocboy3/OCAV/dev" .. tostring(path),file)
+end
+
+fs.makeDirectory("/.OCAV/")
+fs.makeDirectory("/.OCAV/Libraries/")
+dl("/OS.lua","/OS.lua")
+dl("/Antivirus.lua","/.OCAV/Libraries/Antivirus.lua")
+GUI.alert("Sucessfully installed OCAV developer beta.\nA full system reboot is required in order to start the anti-virus.")
